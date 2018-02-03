8d853b53133c77502279a9539201ccab1083197f
Handy Lim <18360062+handylim@users.noreply.github.com>
2018-02-03 19:00:02 +0700

    Create class definitions and helper functions
    Create class definitions for vertex, edge, vector, and rotation
    Create helper functions such as multiplyMatrix(), draw(), clearCanvas(), convertVerticesToMatrix(), convertMatrixToVertices()

--- index.js
+++ index.js
@@ -14,6 +14,100 @@ var canvas                     = document.getElementById('canvas'),
     ],
     screenTransformationResult;
 
+function Vertex (x, y, z) {
+	this.x = x;
+	this.y = y;
+	this.z = z;
+	this.w = 1;
+}
+
+function Edge (startPointIndex, endPointIndex) {
+	this.startPointIndex = startPointIndex;
+	this.endPointIndex = endPointIndex;
+}
+
+function Vector (x, y, z) {
+	this.x = x;
+	this.y = y;
+	this.z = z;
+}
+
+function Rotation (x, y, z) {
+	this.x = x;
+	this.y = y;
+	this.z = z;
+}
+
+function clearCanvas () {
+	context.clearRect(0, 0, canvasWidth, canvasHeight);
+}
+
+function draw () {
+	clearCanvas();
+	for (var i = edges.length - 1; i >= 0; i--) {
+		context.beginPath();
+		context.moveTo(screenTransformationResult[edges[i].startPointIndex].x, screenTransformationResult[edges[i].startPointIndex].y);
+		context.lineTo(screenTransformationResult[edges[i].endPointIndex].x, screenTransformationResult[edges[i].endPointIndex].y);
+		// set line color
+		context.lineWidth = 2;
+		context.strokeStyle = i < 4 ? '#f00' : '#000';
+		context.stroke();
+	}
+}
+
+function convertVerticesToMatrix (vertices) {
+	if (vertices[0].constructor === Array) // check whether vertices is a matrix or not
+		return vertices;
+	else { // vertices is a list of vertices
+		var result = new Array(vertices.length); // initialize array of rows
+		for (i = 0; i < vertices.length; i++) { // convert it to matrix
+			result[i] = new Array(4); // initialize current row with 4 columns for x, y, z, and w
+			result[i][0] = vertices[i].x;
+			result[i][1] = vertices[i].y;
+			result[i][2] = vertices[i].z;
+			result[i][3] = vertices[i].w;
+		}
+
+		return result;
+	}
+}
+
+function convertMatrixToVertices (matrix) {
+	if (matrix[0].constructor === Array) { // check whether matrix is a matrix or not
+		var result = [];
+		for (var i = 0; i < matrix.length; i++) {
+			result.push({
+				            x : matrix[i][0],
+				            y : matrix[i][1],
+				            z : matrix[i][2],
+				            w : matrix[i][3]
+			            });
+
+			if (matrix[i][3] !== 1)
+				console.warn('The w of the vertex is not 1. This point might not be a homogeneous coordinate');
+		}
+
+		return result;
+	}
+	else
+		return matrix;
+}
+
+function multiplyMatrix (a, b) {
+	if (a[0].length !== b.length)
+		throw new Error('Can\'t multiply the matrix');
+
+	var result = new Array(a.length); // initialize array of rows
+	for (var i = 0; i < a.length; i++) {
+		result[i] = new Array(b[0].length).fill(0); // initialize current row with columns with value zeroes
+		for (var j = 0; j < b[0].length; j++)
+			for (var k = 0; k < a[0].length; k++)
+				result[i][j] += a[i][k] * b[k][j];
+	}
+
+	return result;
+}
+
 canvas = $('#canvas');
 context.transform(1, 0, 0, -1, 0, canvasHeight); // to make the origin (0, 0) at the bottom left of the canvas
 
